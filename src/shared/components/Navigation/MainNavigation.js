a1e26bfb811fce3517f63c0c606cd1a0d7b39f7e
qyzhencharles <qyzhencharles@gmail.com>
2023-09-02 20:09:11 -0400

    modal added

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,32 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
 import SideDrawer from './SideDrawer';
-import Backdrop from './Backdrop';
+import Backdrop from '../UIElements/Backdrop';
 import './MainNavigation.css';
-import { useState } from 'react/cjs/react.production.min';
 
 const MainNavigation = props => {
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
-  const openDrawerHandler = props => {
+
+  const openDrawerHandler = () => {
     setDrawerIsOpen(true);
-  }
-  const closeDrawerHandler = props => {
+  };
+
+  const closeDrawerHandler = () => {
     setDrawerIsOpen(false);
-  }
+  };
 
   return (
     <React.Fragment>
       {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
       <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
-        <nav className='main-navigation__drawer-nav'>
+        <nav className="main-navigation__drawer-nav">
           <NavLinks />
         </nav>
       </SideDrawer>
+
       <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
           <span />
           <span />
           <span />
@@ -34,7 +36,7 @@ const MainNavigation = props => {
         <h1 className="main-navigation__title">
           <Link to="/">YourPlaces</Link>
         </h1>
-        <nav className='main-navigation__header-nav'>
+        <nav className="main-navigation__header-nav">
           <NavLinks />
         </nav>
       </MainHeader>
